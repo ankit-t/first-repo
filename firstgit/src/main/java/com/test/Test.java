3a51d42e972b7966e5a50d831327a69fae66060c
ankit-t <47849192+ankit-t@users.noreply.github.com>
2019-06-28 14:55:30 +0530

    removed all ...

--- firstgit/src/main/java/com/test/Test.java
+++ firstgit/src/main/java/com/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
      System.out.println("testing");
      System.out.println("extra code");
-	System.out.println("3rd sop...");
+	System.out.println("3rd sop");
 		for(int i=1;i<=10;i++){
 			System.out.println(i);
 		}
