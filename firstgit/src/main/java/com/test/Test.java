68d25932efe0bd013ba65a72d6c82e5e24d92d67
ANKIT <ankit.991100@gmail.com>
2019-06-28 14:54:49 +0530

    removed ... from sop

--- firstgit/src/main/java/com/test/Test.java
+++ firstgit/src/main/java/com/test/Test.java
@@ -3,7 +3,7 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-     System.out.println("testing...");
+     System.out.println("testing");
      System.out.println("extra code");
 	System.out.println("3rd sop...");
 		for(int i=1;i<=10;i++){
