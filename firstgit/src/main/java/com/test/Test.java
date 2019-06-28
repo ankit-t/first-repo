fd9c21d8dce169536621d7dff343d9dff127bde0
ankit-t <47849192+ankit-t@users.noreply.github.com>
2019-06-28 14:59:05 +0530

    added ... for every sop

--- firstgit/src/main/java/com/test/Test.java
+++ firstgit/src/main/java/com/test/Test.java
@@ -3,13 +3,13 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-     System.out.println("testing");
-     System.out.println("extra code");
-	System.out.println("3rd sop");
+     System.out.println("testing...");
+     System.out.println("extra code...");
+	System.out.println("3rd sop...");
 		for(int i=1;i<=10;i++){
-			System.out.println(i);
+			System.out.println(i...);
 		}
-		System.out.println("end of example");
+		System.out.println("end of example...");
 	}
 
 }
