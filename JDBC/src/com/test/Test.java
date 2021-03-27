97b99420adf93d1bef5d87d2ba85b53b9ae067dd
ANKIT <ANKIT@LAPTOP-VQLFQVTK>
2021-03-27 16:43:03 +0530

    committing test.java from eclipse with 3rd cahnge

--- JDBC/src/com/test/Test.java
+++ JDBC/src/com/test/Test.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 public class Test {
 	// some code from git 
 	//again changed in git itself
+	// 3rd from eclipse
 	public static void main(String[] args) throws Exception {
 		String driver="oracle.jdbc.OracleDriver";
 		String jdbc_url="jdbc:oracle:thin:@localhost:1521:XE";
